6e3824f58db37ff4e15d05d112356ec2aab8e452
Владислав Якимук <Tyuiu.YakimukVV@outlook.com>
2024-09-18 20:02:10 +0500

    Add project files.

---
+++ Tyuiu.YakimukVV.Sprint1.Task0.V15.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.YakimukVV.Sprint1.Task0.V15.Lib
+{
+    public class DataService : ISprint1Task0V0
+    {
+        public double Calculate()
+        {
+            return 20 - (2 * 2 - 8);
+        }
+    }
+}
