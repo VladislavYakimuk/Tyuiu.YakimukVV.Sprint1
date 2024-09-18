3e474bf292e314fd419248501a6266460ed83e16
Владислав Якимук <Tyuiu.YakimukVV@outlook.com>
2024-09-18 20:09:49 +0500

    Task0

--- Tyuiu.YakimukVV.Sprint1.Task0.V15.Lib/DataService.cs
+++ Tyuiu.YakimukVV.Sprint1.Task0.V15.Lib/DataService.cs
@@ -1,7 +1,7 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint1;
 namespace Tyuiu.YakimukVV.Sprint1.Task0.V15.Lib
 {
-    public class DataService : ISprint1Task0V0
+    public class DataService : ISprint1Task0V15
     {
         public double Calculate()
         {
